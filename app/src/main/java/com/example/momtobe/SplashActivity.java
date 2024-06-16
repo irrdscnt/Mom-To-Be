335d7f93bb85980bc056cabe55fa1800586efaf2
Aikanysh <lailievaa02@gmail.com>
2024-06-16 20:47:58 +0600

    added gif to the splash screen

--- app/src/main/java/com/example/momtobe/SplashActivity.java
+++ app/src/main/java/com/example/momtobe/SplashActivity.java
@@ -1,23 +1,34 @@
 package com.example.momtobe;
 
 
+import android.animation.ValueAnimator;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ImageView gifImageView = findViewById(R.id.gifImageView);
 
+        Glide.with(this)
+                .asGif()
+                .load(R.raw.momtobe)
+                .into(gifImageView);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
